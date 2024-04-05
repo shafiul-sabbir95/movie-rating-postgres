2ffb3a412b93460bd4dbc716a75c80bb92b5bcf7
shafiul-sabbir95 <mahereenjui84@gmail.com>
2024-04-05 12:58:48 -0700

    redirect issue

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Movie, Rating
 from .forms import RatingForm, MovieForm
